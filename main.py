fa52a889b9532bd5aaf63f7eb4cf6b3fe17b90de
andrewcharles25 <andrewcharles@mec.science>
2019-08-13 14:53:22 -0400

    doesn't work

---
+++ main.py
@@ -0,0 +1,35 @@
+import webapp2
+import os
+import random
+import jinja2
+
+from google.appengine.ext import ndb
+
+the_jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+extensions=['jinja2.ext.autoescape'],autoescape=True)
+
+class ImageInfomations(ndb.Model):
+    id=ndb.IntegerProperty(required=True)
+    image_url= ndb.StringProperty(required=True)
+    url= ndb.StringProperty(required=True)
+class Home(webapp2.RequestHandler):
+    def get(self):
+        homepage = the_jinja_env.get_template('test.html')
+        Q = ImageInfomations.query().fetch()
+
+        self.response.write(homepage.render({"Q": Q }))
+class AddImage(webapp2.RequestHandler):
+    def post(self):
+        homepage = the_jinja_env.get_template('/addImage/addImage.html')
+        id = self.request.get('location')
+        image_url= self.request.get('image_url')
+        url= self.request.get('url')
+
+        ImageInfomations(image_url=image_url, url=url).put()
+
+        dict= {'url':url, 'image_url':image_url}
+        self.response.write(homepage.render(dict))
+app = webapp2.WSGIApplication([
+('/',Home),
+('/addImage',AddImage)
+], debug=True)
