ca53667197bd7cd8d87a6c096e5bfb392586cc19
hello world <darrenzou1221@gmail.com>
2019-08-14 14:59:01 -0400

    add image page deosn't work

--- app_models.py
+++ app_models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 
 class ImageInfo(ndb.Model):
-    id=ndb.StringProperty(required=True)
-    image_url= ndb.StringProperty(required=True,default= 'https://media3.giphy.com/media/3ornkaYb0ezq88cE5a/giphy.gif')
-    url= ndb.StringProperty(required=True,default= 'https://www.reddit.com/r/dankmemes/')
+    location=ndb.StringProperty(required=True)
+    image_url= ndb.StringProperty(required=False,default= 'https://media3.giphy.com/media/3ornkaYb0ezq88cE5a/giphy.gif')
+    url= ndb.StringProperty(required=False,default= 'https://www.reddit.com/r/dankmemes/')
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import jinja2
 from database import defaultdatas
 from app_models import ImageInfo
 
+
 the_jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 extensions=['jinja2.ext.autoescape'],autoescape=True)
 
@@ -13,29 +14,39 @@ def GetImages():
     return ImageInfo.query().fetch()
 
 def getImages(mosaic_location):
-    imageURL = ImageInfo.query(ImageInfo.id==mosaic_location).get().image_url
-    websiteUrl=ImageInfo.query(ImageInfo.id==mosaic_location).get().url
+    imageURL = ImageInfo.query(ImageInfo.location==mosaic_location).get().image_url
+    websiteUrl=ImageInfo.query(ImageInfo.location==mosaic_location).get().url
 
     images={'imageUrl':imageURL,'websiteUrl':websiteUrl}
     return images
 
 class Home(webapp2.RequestHandler):
     def get(self):
-        defaultdatas()
         homepage = the_jinja_env.get_template('/templates/home.html')
         self.response.write(homepage.render({"images":GetImages()}))
        
+class loadImages(webapp2.RequestHandler):
+    def get(self):
+        defaultdatas()
 class AddImage(webapp2.RequestHandler):
     def get(self):
         homepage = the_jinja_env.get_template('/templates/addImage.html')
-        id = self.request.get('location')
-        image_url= self.request.get('image_url')
-        url= self.request.get('url')
-        imageInfomations(id = id,image_url=image_url, url=url).put()
-        
         self.response.write(homepage.render())
+        location = self.request.get('location')
+    def post(self):   
+        imageurl= self.request.get('image_url')
+        URL= self.request.get('url')
+        print location
+        newImage=ImageInfo.query(ImageInfo.location==location).get()
+        newImage.image_url=imageurl
+        newImage.url=URL
+        newImage.put()
+        
+        
+       
 app = webapp2.WSGIApplication([
 ('/',Home),
-('/addImage',AddImage)
+('/addImage',AddImage),
+('/load', loadImages)
 ], debug=True)
 
