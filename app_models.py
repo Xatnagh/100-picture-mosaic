02c38426ea37a265330a44dd6d8915ae8329e709
hello world <darrenzou1221@gmail.com>
2019-08-14 16:32:06 -0400

    work on data base start method

--- app_models.py
+++ app_models.py
@@ -1,6 +1,6 @@
 from google.appengine.ext import ndb
 
 class ImageInfo(ndb.Model):
-    location=ndb.StringProperty(required=True)
+    location=ndb.IntegerProperty(required=True)
     image_url= ndb.StringProperty(required=False,default= 'https://media3.giphy.com/media/3ornkaYb0ezq88cE5a/giphy.gif')
     url= ndb.StringProperty(required=False,default= 'https://www.reddit.com/r/dankmemes/')
