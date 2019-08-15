135c1c5e01fbd2b0fb85d5e908a15eb9a0609bad
hello world <darrenzou1221@gmail.com>
2019-08-15 10:27:01 -0400

    front end now

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from app_models import ImageInfo, ANCESTORY_KEY
-a=0    
+
 def defaultdatas():
-    global a
-    if a==0:
-        for i in range(1,101):
-            ImageInfo(parent=ANCESTORY_KEY,location=i).put()
-        a=1
-    
+        a=0
+        a=ImageInfo.query().fetch(2)
+        if not a:
+            for i in range(1,101):
+                ImageInfo(parent=ANCESTORY_KEY,location=i).put()
