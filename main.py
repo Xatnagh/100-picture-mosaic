d56262a398891956f76ebddf94250938957f08af
jasonjiangstuy <jasonjiangny@gmail.com>
2019-08-18 23:35:32 -0400

    jason working backend taking user id

--- main.py
+++ main.py
@@ -27,36 +27,35 @@ class LoginPage(webapp2.RequestHandler):
         self.response.write(homepage.render())
 
     def post(self):
-        from flask import request
-        #retreve token
-        token = request.form["id_token"]
 
 
-        from google.oauth2 import id_token
-        from google.auth.transport import requests
+        try:
+            from flask import request
+            #retreve token
+            token = request.form["id_token"]
+            from google.oauth2 import id_token
+            from google.auth.transport import requests
+            # Specify the CLIENT_ID of the app that accesses the backend:
+            idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
 
+            # Or, if multiple clients access the backend server:
+            # idinfo = id_token.verify_oauth2_token(token, requests.Request())
+            # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
+            #     raise ValueError('Could not verify audience.')
 
-try:
-    # Specify the CLIENT_ID of the app that accesses the backend:
-    idinfo = id_token.verify_oauth2_token(token, requests.Request(), CLIENT_ID)
+            if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
+                raise ValueError('Wrong issuer.')
 
-    # Or, if multiple clients access the backend server:
-    # idinfo = id_token.verify_oauth2_token(token, requests.Request())
-    # if idinfo['aud'] not in [CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]:
-    #     raise ValueError('Could not verify audience.')
+            # If auth request is from a G Suite domain:
+            # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
+            #     raise ValueError('Wrong hosted domain.')
 
-    if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-        raise ValueError('Wrong issuer.')
+            # ID token is valid. Get the user's Google Account ID from the decoded token.
+            userid = idinfo['sub']
+        except ValueError:
 
-    # If auth request is from a G Suite domain:
-    # if idinfo['hd'] != GSUITE_DOMAIN_NAME:
-    #     raise ValueError('Wrong hosted domain.')
-
-    # ID token is valid. Get the user's Google Account ID from the decoded token.
-    userid = idinfo['sub']
-except ValueError:
-    # Invalid token
-    pass
+            # Invalid token
+            pass
 
 class AddImage(webapp2.RequestHandler):
     def get(self):
